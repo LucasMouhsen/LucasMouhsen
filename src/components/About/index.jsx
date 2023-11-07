44dd29ee0c4d8584bdc2ca1b65a01ca462dc92b4
lucasmouhsen <lucasmouhsen@gmail.com>
2023-11-07 16:33:22 -0300

    Nuevos estilos y foto sacada

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,13 +4,13 @@ export default function About() {
     return (
         <div className={styles.box} id="about">
             <div className={styles.boxAbout}>
-                <h1 className={styles.name}>Lucas Diaz Mouhsen</h1>
-                <p className={styles.About}>Analista de Datos y Procesos en la Municipalidad de San Miguel, con 26 años de edad y una formación autodidacta en programación y complementada con un curso intensivo de desarrollador full stack en Digital House.</p>
+                <div className={styles.nameAbout}>
+                    <h1 className={styles.name}>Lucas Diaz Mouhsen</h1>
+                    <p className={styles.about}>Analista de Datos y Procesos en la Municipalidad de San Miguel, con 26 años de edad y una formación autodidacta en programación y complementada con un curso intensivo de desarrollador full stack en Digital House.</p>
+                </div>
+                <div className={styles.boxImg}>
+                </div>
             </div>
-            <div className={styles.boxImg}>
-                <img src="/images/yo.png" width={100} alt="Lucas Diaz Mouhsen" />
-            </div>
-
         </div>
     )
 }
