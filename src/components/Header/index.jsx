86f3670b98bee97389803f8ec1b50f16e5f2218b
lucasmouhsen <lucasmouhsen@gmail.com>
2023-11-07 16:37:44 -0300

    Estilo heder

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,19 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-/* import styles from "./index.css" */
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun } from "@fortawesome/free-regular-svg-icons"
+import './index.css'
 
 export default function Header() {
     return (
-        <Navbar expand="sm" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#About">About</Nav.Link>
-                        <Nav.Link href="#Proyects">Proyects</Nav.Link>
-                        <Nav.Link href="#Skills">Skills</Nav.Link>
-                        <Nav.Link href="#Contact">Contact</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div className='header'>
+            <ul className='headerList'>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <a href="#" className='mode'>
+                <FontAwesomeIcon icon={faSun} />
+            </a>
+        </div>
     );
 }
