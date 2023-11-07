d8ef964dbb6d5414abcad1face90bb3a087050de
lucasmouhsen <lucasmouhsen@gmail.com>
2023-11-07 16:38:09 -0300

    Base de estilos Projects

--- src/components/Proyects/index.jsx
+++ src/components/Proyects/index.jsx
@@ -1,18 +1,25 @@
-
 import styles from "./index.module.css"
+
 export default function Proyects() {
     return (
-        <div className={styles.box} id="proyect">
+        <div className={styles.boxProyects} id="projects" >
             <div className={styles.boxAbout}>
-                <h1 className={styles.name}>Proyectos</h1>
+                <h1 className={styles.name}>PROYECTOS</h1>
             </div>
             <div className={styles.boxImg}>
-                <img src="/images/yo.png" width={100} alt="Lucas Diaz Mouhsen" />
-                <img src="/images/yo.png" width={100} alt="Lucas Diaz Mouhsen" />
-                <img src="/images/yo.png" width={100} alt="Lucas Diaz Mouhsen" />
-                <img src="/images/yo.png" width={100} alt="Lucas Diaz Mouhsen" />
+                <div>
+                    <img src="/images/" width={100} alt="React Drinks" />
+                    <p>React Drinks</p>
+                </div>
+                <div>
+                    <img src="/images/" width={100} alt="ALi Music" />
+                    <p>ALi Music</p>
+                </div>
+                <div>
+                    <img src="/images/" width={100} alt="Registo HC" />
+                    <p>Registo HC</p>
+                </div>
             </div>
-
         </div>
     )
 }
