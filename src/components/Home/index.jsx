a80efa351deb651c1fb5f76c46fb3c873f048133
Lucas12github <Lucasmouhsen@gmail.com>
2024-01-25 18:32:53 -0300

    react-simple-typewriter para el efecto de escritura

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,32 @@
+import styles from "./index.module.css";
+import { Typewriter } from 'react-simple-typewriter';
 
-import styles from "./index.module.css"
-export default function Home() {
+const Home = () => {
     return (
-        <div className={styles.box} id="home">
+        <section className={styles.box} id="home">
             <div className={styles.boxHome}>
                 <div className={styles.nameHome}>
-                    <h1 className={styles.name}>Lucas Diaz Mouhsen</h1>
-                    <p className={styles.Home}>Desarrollador.</p>
+                    <h1 className={styles.name}>
+                        <span>Lucas Diaz Mouhsen</span>
+                    </h1>
+                    <p className={styles.description}>Desarrollador {' '}
+                        <Typewriter
+                            words={['FrontEnd.', 'BackEnd.', 'Full Stack.']}
+                            loop={100}
+                            cursor
+                            cursorStyle='|'
+                            typeSpeed={100}
+                            deleteSpeed={50}
+                            className={styles.writer}
+                        />
+                    </p>
                 </div>
                 <div className={styles.boxImg}>
+                    <img className={styles.img} src="images\linkedin.png" alt="" />
                 </div>
             </div>
-        </div>
-    )
-}
+        </section>
+    );
+};
+
+export default Home;
