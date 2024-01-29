12f19a9f15101163c41113965d9e83de051db69f
Lucas12github <Lucasmouhsen@gmail.com>
2024-01-28 22:02:56 -0300

    Nombres de skills quitados

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -4,36 +4,36 @@ export default function Skills() {
     return (
         <section className={styles.boxSkills} id="skills">
             <div className={styles.boxAbout}>
-                <h1 className={styles.name}>Skills</h1>
+                <h1 className={styles.name}>HABILIDADES</h1>
             </div>
             <div className={styles.boxImg}>
                 <div>
                     <img src="images/react.svg" width={100} alt="React" />
-                    <p>REACT</p>
+                    {/* <p>REACT</p> */}
                 </div>
                 <div>
                     <img src="images/node-js.svg" width={100} alt="Node JS" />
-                    <p>NODE JS</p>
+                    {/* <p>NODE JS</p> */}
                 </div>  
                 
                 <div>
                     <img src="images/python.svg" width={100} alt="Python" />
-                    <p>PYTHON</p>
+                    {/* <p>PYTHON</p> */}
                 </div>
                 
                 <div>
                     <img src="images/mysql.svg" width={100} alt="My Sql" />
-                    <p>MY SQL</p>
+                    {/* <p>MY SQL</p> */}
                 </div>
                 
                 <div>
                     <img src="images/git.svg" width={100} alt="Git" />
-                    <p>GIT</p>
+                    {/* <p>GIT</p> */}
                 </div>
                 
                 <div>
                     <img src="images/powerbi.png" width={100} alt="Power Bi" />
-                    <p>POWER BI</p>
+                    {/* <p>POWER BI</p> */}
                 </div>
             </div>
 
